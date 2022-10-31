99f73dfada1756f2c0958e3026e09b406aaa961c
Darren Lee <danalee@ucsc.edu>
2022-10-30 18:44:34 -0700

    Upload New File

---
+++ project/MWST.py
@@ -0,0 +1,31 @@
+#!/usr/local/bin/python3
+# your python code starts here
+
+import heapq
+
+num_vertices=input()
+num_edges=input()
+
+adj={ i:[] for i in range(num_vertices) }
+
+for _ in range(num_edges):
+    weight, i, j=input()
+    adj[i].append(weight,j)
+    adj[j].append(weight,i)
+
+total_weight=0
+visit=set()
+minH=[[0,0]]
+
+while len(visit)<num_vertices:
+    cost,i=heapq.heappop(minH)
+    if i in visit:
+        continue
+
+    total_weight+=cost
+    visit.add(i)
+    for neiCost,nei in adj[i]:
+        if nei not in visit:
+            heapq.heappush(minH,[neiCost,nei])
+
+print("Total Weight =", total_weight)
